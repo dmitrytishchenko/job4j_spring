7a28b4ae1ed659c86a63cca5b77dbbfcd6b590da
dmitrytishchenko <tdmitiu@mail.ru>
2020-09-20 21:08:43 +0300

    1. Продемонстрировать различные виды создания spring beans. [#217964]

---
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -0,0 +1,19 @@
+package ru.job4j.di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(StartUI.class);
+        context.register(ConsoleInput.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add("Petr Arsentev");
+        ui.add("Dmitry Tishchenko");
+        ui.print();
+        String number = ui.askString("Input id :");
+        System.out.println(String.format("This number was entered %s", ui.askInt(number)));
+    }
+}
